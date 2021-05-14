768b2985a3def81423b56373230d3cfdf6a33d8c
Greg Albrecht <github.com@undef.net>
2021-05-14 15:12:45 -0700

    Version Bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import sys
 import setuptools
 
 __title__ = "pytak"
-__version__ = "3.5.0b6"
+__version__ = "3.5.0"
 __author__ = "Greg Albrecht W2GMD <oss@undef.net>"
 __copyright__ = "Copyright 2021 Orion Labs, Inc."
 __license__ = "Apache License, Version 2.0"
