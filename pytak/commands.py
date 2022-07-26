28a73bc7628e711ff377dd73320b07037fcb62f9
Greg Albrecht <github.com@undef.net>
2022-07-26 11:57:32 -0700

    added boilerplate commands file

---
+++ pytak/commands.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Greg Albrecht <oss@undef.net>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author:: Greg Albrecht W2GMD <oss@undef.net>
+#
+
+"""PyTAK Command Line."""
+
+import pytak
+
+__author__ = "Greg Albrecht W2GMD <oss@undef.net>"
+__copyright__ = "Copyright 2022 Greg Albrecht"
+__license__ = "Apache License, Version 2.0"
+
+
+def main() -> None:
+    """Main function."""
+    # PyTAK CLI tool boilerplate:
+    pytak.cli(__name__.split(".", maxsplit=1)[0])
+
+
+if __name__ == "__main__":
+    main()
