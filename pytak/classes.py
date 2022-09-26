525f38d18a919e952c1aed5a082d8bcd73e44745
peterQFR <petermilani80@gmail.com>
2022-09-26 13:01:31 +1000

    Remove added line

--- pytak/classes.py
+++ pytak/classes.py
@@ -57,8 +57,7 @@ class Worker:  # pylint: disable=too-few-public-methods
         else:
             config_p = ConfigParser({})
             config_p.add_section("pytak")
-            self.config = config_p["pytak"]
-        
+            self.config = config_p["pytak"] 
         if self.config.getboolean("DEBUG", False):
             _ = [x.setLevel(logging.DEBUG) for x in self._logger.handlers]
 
