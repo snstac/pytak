8862734a4e63feca7dc3cee724f2d7f5caa3b7fd
Greg Albrecht <github.com@undef.net>
2023-01-19 19:28:41 -0800

    Fighting CI

--- pytak/__init__.py
+++ pytak/__init__.py
@@ -65,6 +65,8 @@ from .client_functions import (  # NOQA
 
 from . import asyncio_dgram  # NOQA
 
+# from .crypto_functions import *
+
 __author__ = "Greg Albrecht W2GMD <oss@undef.net>"
 __copyright__ = "Copyright 2023 Greg Albrecht"
 __license__ = "Apache License, Version 2.0"
--- pytak/client_functions.py
+++ pytak/client_functions.py
@@ -311,9 +311,9 @@ def read_pref_package(pref_package: str) -> dict:
     client_password: str = prefs.get("client_password", "")
     assert client_password
 
-    import pytak.crypto_functions as cfunc
+    # import pytak.crypto_functions as cfunc
 
-    pem_certs: dict = cfunc.convert_cert(cert_location, client_password)
+    pem_certs: dict = pytak.crypto_functions.convert_cert(cert_location, client_password)
     pref_config["PYTAK_TLS_CLIENT_CERT"] = pem_certs.get("cert_pem_path")
     pref_config["PYTAK_TLS_CLIENT_KEY"] = pem_certs.get("pk_pem_path")
     pref_config["PYTAK_TLS_CLIENT_CAFILE"] = pem_certs.get("ca_pem_path")
