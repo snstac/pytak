386f8e75b5e5e9c7063a5cb20e81f31bdfcf37dc
Joe Dinius <josephwdinius@gmail.com>
2024-11-25 17:14:23 -0800

    Allow tests to be run from different folder than pytak root

--- tests/test_pref_packages.py
+++ tests/test_pref_packages.py
@@ -28,18 +28,19 @@ __author__ = "Greg Albrecht <gba@snstac.com>"
 __copyright__ = "Copyright Sensors & Signals LLC https://www.snstac.com"
 __license__ = "Apache License, Version 2.0"
 
+__folder__ = os.path.dirname(__file__)
 
 def test_load_preferences() -> None:
     """Test loading a preferences file."""
-    test_pref: str = "tests/data/test_pref.pref"
-    prefs: dict = pytak.functions.load_preferences(test_pref, "tests/data")
+    test_pref: str = __folder__ + "/data/test_pref.pref"
+    prefs: dict = pytak.functions.load_preferences(test_pref, __folder__ + "/data")
     assert all(prefs)
 
 
 def test_load_connectString2url() -> None:
     """Test converting a TAK connectString to a URL"""
-    test_pref: str = "tests/data/test_pref.pref"
-    prefs: dict = pytak.functions.load_preferences(test_pref, "tests/data")
+    test_pref: str = __folder__ + "/data/test_pref.pref"
+    prefs: dict = pytak.functions.load_preferences(test_pref, __folder__ + "/data")
     connect_string: str = prefs.get("connect_string")
     url: str = pytak.functions.connectString2url(connect_string)
     assert url == "ssl://takserver.example.com:8089"
@@ -47,15 +48,15 @@ def test_load_connectString2url() -> None:
 
 def test_load_cert() -> None:
     cert: list = pytak.crypto_functions.load_cert(
-        "tests/data/test_user_cert.p12", "atakatak"
+        __folder__ + "/data/test_user_cert.p12", "atakatak"
     )
     assert len(cert) == 3
 
 
 def test_load_convert_cert():
     """Test converting P12 certs to a PEM certs."""
-    test_pref: str = "tests/data/test_pref.pref"
-    prefs: dict = pytak.functions.load_preferences(test_pref, "tests/data")
+    test_pref: str = __folder__ + "/data/test_pref.pref"
+    prefs: dict = pytak.functions.load_preferences(test_pref, __folder__ + "/data")
 
     client_password: str = prefs.get("client_password")
     assert client_password
@@ -76,23 +77,23 @@ def test_load_convert_cert():
     assert os.path.exists(cert_pem_path)
     assert os.path.exists(ca_pem_path)
 
-    with open("tests/data/test_pk.pem", "rb+") as tpk_fd:
+    with open(__folder__ + "/data/test_pk.pem", "rb+") as tpk_fd:
         test_pk = tpk_fd.read()
         with open(pk_pem_path, "rb+") as pk_fd:
             assert pk_fd.read() == test_pk
 
-    with open("tests/data/test_user_cert.pem", "rb+") as tc_fd:
+    with open(__folder__ + "/data/test_user_cert.pem", "rb+") as tc_fd:
         test_cert = tc_fd.read()
         with open(cert_pem_path, "rb+") as ck_fd:
             assert ck_fd.read() == test_cert
 
-    with open("tests/data/test_ca_cert.pem", "rb+") as tc_fd:
+    with open(__folder__ + "/data/test_ca_cert.pem", "rb+") as tc_fd:
         test_cert = tc_fd.read()
         with open(ca_pem_path, "rb+") as ck_fd:
             assert ck_fd.read() == test_cert
 
 
 def test_read_read_pref_package():
-    pref_package = "tests/data/test_pref_package.zip"
+    pref_package = __folder__ + "/data/test_pref_package.zip"
     prefs = pytak.client_functions.read_pref_package(pref_package)
     assert all(prefs)
